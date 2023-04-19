531a744d99c794e8073f67622de426fe60e89ef5
seungwookim <sonicdx886@gmail.com>
2023-04-19 18:42:02 +0900

    merge

--- assignment/asg1/merge/merge.c
+++ assignment/asg1/merge/merge.c
@@ -1,26 +1,40 @@
 #include "../default.h"
 
-int get_storage_table_info(){
-    DIR *dir;
-    struct dirent *ent;
-    mkdir( "storage_table", 0755);
-    dir = opendir ("./storage_table");
-    if (dir != NULL) {
-        /* print all the files and directories within directory */
-        while ((ent = readdir (dir)) != NULL) {
-            printf ("%s\n", ent->d_name);
-            if (strcmp(".", ent->d_name) == 0) {
-                printf("its .\n");
-            }
-        }
-        closedir (dir);
+// int get_storage_table_info(){
+//     DIR *dir;
+//     struct dirent *ent;
+//     mkdir( "storage_table", 0755);
+//     dir = opendir ("./storage_table");
+//     if (dir != NULL) {
+//         /* print all the files and directories within directory */
+//         while ((ent = readdir (dir)) != NULL) {
+//             printf ("%s\n", ent->d_name);
+//             if (strcmp(".", ent->d_name) == 0) {
+//                 printf("its .\n");
+//             }
+//         }
+//         closedir (dir);
+//     } else {
+//          /* could not open directory */
+//          perror ("");
+//          printf("no open!\n");
+//         return EXIT_FAILURE;
+//     }
+//     return 0;
+// }
+
+/* util functions */
+int compare(const void *a, const void *b) {
+    /* compare filename (ex. 1.stt < 2.stt)*/
+    int num_a = atoi(*(char**)a);
+    int num_b = atoi(*(char**)b);
+    if (num_a < num_b) {
+        return -1;
+    } else if (num_a > num_b) {
+        return 1;
     } else {
-         /* could not open directory */
-         perror ("");
-         printf("no open!\n");
-        return EXIT_FAILURE;
+        return 0;
     }
-    return 0;
 }
 
 int get_storage_table_number() {
@@ -95,6 +109,113 @@ void flush_kvs() {
     return;
 }
 
+void get_table_names(char files[3][MAX_TABLE_NAME_SIZE]){
+    mkdir("storage_table", 0755);
+    struct dirent *ent;
+    DIR *dir;
+    dir = opendir ("./storage_table");
+    if (dir == NULL) {
+        printf("cannot open storage_table\n");
+        return;
+    }
+    char *ptr;
+    int idx = 0;
+    while((ent = readdir(dir)) != NULL) {
+        // . or .. is not storage table
+        if ((strcmp(".", ent->d_name) == 0) || (strcmp("..", ent->d_name) == 0)) continue;
+        printf("[[%s]]\n", ent->d_name);
+        strcpy(files[idx++], ent->d_name);
+    }
+    return;
+}
+
+void merge_storage_tables(){
+    int i, j;
+    printf("get new idx\n");
+    int merged_table_idx = get_new_storage_table_index();
+    printf("get table num\n");
+    int table_num = get_storage_table_number();
+
+    /* get tables name */
+    char files_arr[3][MAX_TABLE_NAME_SIZE] = {{'\0',},{'\0',},{'\0',}};
+    printf("'get table names\n'");
+    get_table_names(files_arr);
+    
+    printf("file[0] : %s, file[1]: %s\n", files_arr[0], files_arr[1]);
+    /* sort by table name number (ex. 1.stt < 2.stt) */
+    char *files[3];
+    for (i = 0 ; i < 3 ; i ++) {
+        files[i] = files_arr[i];
+    }
+    
+    printf("'sort table names\n'");
+    qsort(files, table_num, sizeof(char *), compare);
+
+    /* save past 2 tables info in structure */
+    storage_file targets[2];
+    targets[0].num = targets[1].num = 0;
+    FILE *fp;
+    char read_buf[100];
+    char *line, *word;
+    for (i=0;i<2;i++){
+        printf("'read data'\n");
+        char curr_filename[MAX_TABLE_NAME_SIZE] = {'\0',};
+        sprintf(curr_filename, "./storage_table/%s", files[i]);
+        fp = fopen(curr_filename, "r");
+        if (!fp) {
+            printf("file open error : %s\n", files[i]);
+            return;
+        }
+        strcpy(targets[i].file_name, files[i]);
+        while (!feof(fp)) {
+            printf("ok %d\n", i);
+            int idx = targets[i].num;
+            line = fgets(read_buf, 100, fp);
+            if (!line) {
+                break;
+            }
+            word = strtok(line, " "); // order of data
+            word = strtok(NULL, " "); // key
+            targets[i].keys[idx] = atoi(word);
+            word = strtok(NULL, " "); // value
+            strcpy(targets[i].values[idx], word);
+            targets[i].num++;
+        }
+        fclose(fp);
+    }
+
+    printf("scan two tables done\n");
+
+    /* merge two tables */
+    storage_file merged;
+    merged.num = 0;
+    memset(merged.keys, 0, sizeof(merged.keys)); // initialize
+    for(i=0;i<2;i++) {
+        printf("to merged :%d\n",i);
+        int num = targets[i].num;
+        printf("NUM: %d\n", num);
+        for(j=0;j<num;j++){
+            int key = targets[i].keys[j];
+            printf("[%d] keys:%d\n",j,key);
+            merged.keys[key] = key;
+            strcpy(merged.values[key], targets[i].values[j]);
+            merged.num++;
+        }
+    }
+
+    /* write */
+    printf("now write\n");
+    char merged_filename[MAX_TABLE_NAME_SIZE] = {'\0',};
+    sprintf(merged_filename, "./storage_table/%d.stt", merged_table_idx);
+    fp = fopen(merged_filename, "w");
+    int order = 1;
+    for(i=0;i<MAX_KEY_VALUE;i++){
+        if(merged.keys[i] == 0) continue;
+        fprintf(fp,"%d %d %s", order++, merged.keys[i], merged.values[i]);
+    }
+    fclose(fp);
+}
+
 void merge_process() {
     mkdir("storage_table", 0755);
     while(true) {
@@ -102,16 +223,23 @@ void merge_process() {
             case REQ_NONE:
                 break;
             case REQ_FLUSH:
-                printf("REQ_FLUSH START\n");
-                //get_storage_table_info();
                 create_new_storage_table_with_kvs();
-                printf("NOW FLUSH\n");
                 flush_kvs();
-                shm_merge->request = REQ_NONE;
-                printf("REQ_FLUSH DONE[MERGE]\n");
-                semop(sem_kvs_id, &v_kvs[0], 1);
-                break;
+
+                if (get_storage_table_number() == 3) {
+                    /* have to merge */
+                    printf("have to merge\n");
+                    shm_merge->request = REQ_MERGE;
+                } else {
+                    shm_merge->request = REQ_NONE;
+                    semop(sem_kvs_id, &v_kvs[0], 1);
+                    break;
+                }
             case REQ_MERGE:
+                printf("merge start (REQ_MERGE)\n");
+                merge_storage_tables();
+                shm_merge->request = REQ_NONE;
+                semop(sem_kvs_id, &v_kvs[0], 1);
                 break;
             default:
                 break;
--- assignment/asg1/merge/merge.h
+++ assignment/asg1/merge/merge.h
@@ -4,5 +4,15 @@
 /* util */
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define MAX_TABLE_NAME_SIZE 20
+#define MAX_STORAGE_DATA_NUM 10000
+#define MAX_KEY_VALUE 10000
+
+/* file */
+typedef struct _stroage_file {
+    int num;
+    int keys[MAX_STORAGE_DATA_NUM];
+    char values[MAX_STORAGE_DATA_NUM][LCD_MAX_BUFF];
+    char file_name[MAX_TABLE_NAME_SIZE];
+} storage_file;
 
 #endif
